37cf7a84c6ccf3b30e2c20fa83a67913d0b8b4de
Elliot Saba <staticfloat@gmail.com>
2019-09-02 22:58:05 -0700

    PCRE2_jll build 10.31.0+0

---
+++ src/PCRE2_jll.jl
@@ -0,0 +1,26 @@
+module PCRE2_jll
+using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
+import Base: UUID
+
+# Load Artifacts.toml file
+artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
+
+# Extract all platforms
+artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("efcefdf7-47ab-520b-bdef-62a2eaa19f15"))
+platforms = [Pkg.Artifacts.unpack_platform(e, "PCRE2", artifacts_toml) for e in artifacts["PCRE2"]]
+
+# Filter platforms based on what wrappers we've generated on-disk
+platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
+
+# From the available options, choose the best platform
+best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
+
+# Silently fail if there's no binaries for this platform
+if best_platform === nothing
+    @debug("Unable to load PCRE2; unsupported platform $(triplet(platform_key_abi()))")
+else
+    # Load the appropriate wrapper
+    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
+end
+
+end  # module PCRE2_jll
