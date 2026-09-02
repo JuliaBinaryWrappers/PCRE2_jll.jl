637f347d42dd8cb4c56dc28fb9c5352903c00987
jlbuild <juliabuildbot@gmail.com>
2026-09-02 09:17:29 +0000

    PCRE2_jll build 10.48.0+0

--- src/PCRE2_jll.jl
+++ src/PCRE2_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("PCRE2")
-JLLWrappers.@generate_main_file("PCRE2", UUID("efcefdf7-47ab-520b-bdef-62a2eaa19f15"))
+JLLWrappers.@generate_main_file("PCRE2", Base.UUID("efcefdf7-47ab-520b-bdef-62a2eaa19f15"))
 end  # module PCRE2_jll
